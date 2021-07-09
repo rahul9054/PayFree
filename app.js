2e907dcf6c0a326305bcd1968b53d9df00fa5bec
rahul9054 <rahulkhandelwal9530057219@gmail.com>
2021-07-10 04:13:32 +0530

    deploy

--- app.js
+++ app.js
@@ -88,6 +88,6 @@ app.post("/api/payment/verify", (req, res) => {
    
     res.send(response);
 });
-app.listen("7000", () => {
+app.listen(process.env.PORT || 7000, () => {
   console.log("server started");
 });
